a85e8a638108edf3e0ed34246e067a8a396076d6
Charlietzu <caiocesarsilva08@gmail.com>
2021-05-08 20:47:11 -0300

    Updating role functionality

--- WebEmployee/Controllers/AdministrationController.cs
+++ WebEmployee/Controllers/AdministrationController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using WebEmployee.Models;
 using WebEmployee.ViewModels;
 
 namespace WebEmployee.Controllers
@@ -8,9 +11,11 @@ namespace WebEmployee.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         [HttpGet]
         public IActionResult CreateRole()
@@ -48,6 +53,67 @@ namespace WebEmployee.Controllers
         {
             var roles = _roleManager.Roles;
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"The role with Id = {id} was not found";
+                return View("NotFound");
+            }
+
+            EditRoleViewModel model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            List<ApplicationUser> userList = _userManager.Users.ToList();
+
+            foreach (ApplicationUser user in userList)
+            {
+                // If the user exists in the role, add the user name
+                // to the property Users of EditRoleViewModel
+                // This model object is then displayed
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"The role with Id = {model.Id} was not found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+
+                IdentityResult result = await _roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
         }
     }
 }
---
+++ WebEmployee/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebEmployee.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "The role name is required")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}
